53ec44d41eccd86f8ec73a26b4e7cbfa1f38688b
lihq1403 <lihaiqing1994@163.com>
2019-08-26 18:06:53 +0800

    init api

---
+++ app/Http/Controllers/Api/V1/TestsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Http\Request;
+
+class TestsController extends ApiController
+{
+    public function test()
+    {
+        return 'test';
+    }
+}
