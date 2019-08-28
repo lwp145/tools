9420a12359ed1783f3852f936e041b76f00c3155
lihq1403 <lihaiqing1994@163.com>
2019-08-28 14:37:22 +0800

    修改welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HELLO WORLD
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://lihq.xyz" target="_blank">Blog</a>
+                    <a href="https://github.com/lihq1403" target="_blank">GitHub</a>
                 </div>
             </div>
         </div>
